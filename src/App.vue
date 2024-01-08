4cbf5ee3288bf78e9ff8870e2e0d94db17fe66d5
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-08 22:24:52 +0000

    working with models

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click</button>
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">Info</a>
+      </template>
+      <h1>Sign up for the Giveaway!</h1>
+      <p>Grab our new merch for half the price!</p>
+    </Modal>
+  </teleport>
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal theme="" @close="toggleModalTwo">
+      <h1>Verify your email</h1>
+      <p>Go to your emails and click on the Verify Email link.</p>
+    </Modal>
+  </teleport>
+  <p>Welcome!</p>
+  <div>
+    <button @click="toggleModal">Open Modal</button>
+  </div>
+  <div>
+    <button @click="toggleModalTwo">Open 2nd Modal</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "My First Vue 3",
+  components: { Modal },
+  data() {
+    return {
+      title: "I am the TITLE",
+      modal: {
+        heading: "Sign up for the Giveaway!",
+        text: "Grab our new merch for half the price!",
+      },
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log("clicked", this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -23,4 +72,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+/* h1 {
+  display: inline-block;
+} */
 </style>
